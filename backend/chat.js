35cd6df9a5ae87f6a01299af6972c139bbc89aee
Vimal <vimalkumarak478@gmail.com>
2025-08-30 21:50:33 +0530

    chat LLM groq messege send -vkc

--- backend/chat.js
+++ backend/chat.js
@@ -1,5 +1,6 @@
 import readline from 'node:readline/promises';
 import Groq from "groq-sdk";
+import { vectorStore } from './prepare.js';
 
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
@@ -14,17 +15,33 @@ export async function chat(){
         if(question === 'bye'){
             break;
         }
+
+        // retrieval
+        const relevantChunks = await vectorStore.similaritySearch(question, 3);
+        const context = relevantChunks.map(chunk => chunk.pageContent).join('\n\n');
+
+        const SYSTEM_PROMPT = `You are an assistant for the question-answering taks. Use the following
+        relevent pieces of retrieved context to the answer the question. You don't know the answer, say
+        I don't know.`;
+
+        const userQuery = `Question: ${question}
+        Relevant context: ${context}
+        Answer: `;
+        const completion = await groq.chat.completions
+    .create({
+      messages: [
+        {
+            role: 'system',
+            content: SYSTEM_PROMPT
+        },
+        {
+          role: 'user',
+          content: userQuery,
+        },
+      ],
+      model: "llama-3 3-70b-versatile",
+    });
     }
-    // const completion = await groq.chat.completions
-    // .create({
-    //   messages: [
-    //     {
-    //       role: 'user',
-    //       content: 'Explain the importance of fast language models',
-    //     },
-    //   ],
-    //   model: "llama-3 3-70b-versatile",
-    // });
 
     rl.close();
 };
